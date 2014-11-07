30acf6096d5f06b5871947da66bc14cfbf7af07b
tarzak <jobtarzak@gmail.com>
2014-11-07 23:29:40 +0200

    add index.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express')
   , routes = require('./routes')
   , lists = require('./routes/lists')
-  , reports = require('./routes/reports')
   , http = require('http')
   , path = require('path')
   , mcapi = require('./node_modules/mailchimp-api/mailchimp')
